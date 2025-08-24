469f24022946a2b195c052d1faa41bf1b0cc5bc1
rbgud7957 <r67890@naver.com>
2025-08-24 17:18:51 +0900

    Update Todo.js

--- models/Todo.js
+++ models/Todo.js
@@ -1,9 +1,11 @@
-// models/Todo.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  completed: { type: Boolean, default: false }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  dueDate: { type: Date },
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Todo', todoSchema);
+module.exports = mongoose.model("Todo", todoSchema);
