1aade6f68fb40c3287652d096a6880a5750d1ad6
rbgud7957 <r67890@naver.com>
2025-08-24 17:20:34 +0900

    코드 정리

--- routes/todos.js
+++ routes/todos.js
@@ -1,42 +1,14 @@
 // routes/todos.js
 const express = require('express');
+const { authMiddleware } = require("../middleware/auth");
+const todoController = require("../controllers/todoController");
+
 const router = express.Router();
-const Todo = require('../models/Todo');
 
-// 모든 할 일 가져오기
-router.get('/', async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
-});
-
-// 새 할 일 추가
-router.post('/', async (req, res) => {
-  const newTodo = new Todo({ text: req.body.text });
-  const savedTodo = await newTodo.save();
-  res.json(savedTodo);
-});
-
-// 완료 여부 수정
-router.put('/:id', async (req, res) => {
-  const updatedTodo = await Todo.findByIdAndUpdate(
-    req.params.id,
-    { completed: req.body.completed },
-    { new: true }
-  );
-  res.json(updatedTodo);
-});
-
-// 할 일 삭제
-router.delete('/:id', async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
-  res.json({ message: '삭제 완료' });
-});
+// ✅ 할 일 CRUD 라우트 (모두 로그인 필요)
+router.post("/", authMiddleware(), todoController.createTodo);
+router.get("/", authMiddleware(), todoController.getTodos);
+router.put("/:id", authMiddleware(), todoController.updateTodo);
+router.delete("/:id", authMiddleware(), todoController.deleteTodo);
 
 module.exports = router;
-
-const authMiddleware = require('../middleware/auth');
-
-router.get('/', authMiddleware, async (req, res) => {
-  const todos = await Todo.find({ userId: req.user.userId });
-  res.json(todos);
-});
