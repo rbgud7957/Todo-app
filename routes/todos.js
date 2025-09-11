c593896741f83f3a106d1e2a91d99c1383badccd
rbgud7957 <r67890@naver.com>
2025-09-11 16:42:56 +0900

    Update todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -5,13 +5,13 @@ const todoController = require("../controllers/todoController");
 
 const router = express.Router();
 
-// ✅ 할 일 CRUD 라우트 (모두 로그인 필요)
+// 할 일 CRUD 라우트 (모두 로그인 필요)
 router.post("/", authMiddleware(), todoController.createTodo);
 router.get("/", authMiddleware(), todoController.getTodos);
 router.put("/:id", authMiddleware(), todoController.updateTodo);
 router.delete("/:id", authMiddleware(), todoController.deleteTodo);
 
-// ✅ 통계 API
+// 통계 API
 router.get("/stats", authMiddleware(), todoController.getStats);
 
 module.exports = router;
