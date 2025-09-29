8aeeccfb4b7d14dda1ddb9f3644d69ab084d7afc
rbgud7957 <r67890@naver.com>
2025-09-29 17:50:33 +0900

    비밀번호 변경 추가

--- models/User.js
+++ models/User.js
@@ -1,8 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" }
 });
 
-module.exports = mongoose.model('User', userSchema);
+// 저장 전에 비밀번호 해시
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// 비밀번호 검증 메서드
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
