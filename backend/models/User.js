9fe9a1b664f7a0cb714d24bc17dd39d949129747
rbgud7957 <r67890@naver.com>
2025-10-22 19:28:57 +0900

    토큰 추카

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: false, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" }
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+
+  refreshToken: { type: String, default: null },
 });
 
 // 저장 전에 비밀번호 해시
