a60854a0443b2e97ec69c523d80130d69bd52027
rbgud7957 <r67890@naver.com>
2025-10-27 15:35:38 +0900

    Add files via upload

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "토큰이 없습니다." });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.error("JWT 검증 실패:", err);
+    res.status(403).json({ message: "유효하지 않은 토큰입니다." });
+  }
+};
