6b10858d0ed82cd15ab4f89e948715ea1acba48a
rbgud7957 <r67890@naver.com>
2025-11-29 16:01:43 +0900

    Update axiosInstance.js

--- frontend/app/utils/axiosInstance.js
+++ frontend/app/utils/axiosInstance.js
@@ -1,16 +1,22 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://todo-app-vgyu.onrender.com"; // 
+
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: API_BASE_URL,
+  withCredentials: false,
 });
 
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
   return config;
 });
 
 export default instance;
-
