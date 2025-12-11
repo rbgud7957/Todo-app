95281a41ad99627ce3297185e9ecfbbf52f49a86
rbgud7957 <r67890@naver.com>
2025-12-11 14:06:07 +0900

    관리자 루트 삭제

--- backend/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { authMiddleware } = require("../middleware/auth");
-
-router.get("/dashboard", authMiddleware(["admin"]), (req, res) => {
-  res.send("관리자 대시보드");
-});
-
-module.exports = router;
